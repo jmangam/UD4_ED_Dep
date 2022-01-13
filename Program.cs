65490f7e0442879bc302fa311b7d7d4d01066713
Manzano Gamaza, Javier <94846102+jmangam@users.noreply.github.com>
2022-01-13 13:01:33 +0100

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,12 +1,26 @@
 ﻿using System;
-
-namespace GetStartedDebugging
+class ArrayExample
 {
-    class Program
+    static void Main()
     {
-        static void Main(string[] args)
+        //define un array de caracteres llamado letters
+        //se reserva memoria para los elementos de tipo char
+        char[] letters = { 'f', 'r', 'e', 'd', ' ', 's', 'm', 'i', 't', 'h' };
+        string name = "";
+        //le da el espacio suficiente para poder mostrar el mensaje que queremoss
+        int[] a = new int[10];
+        //bucle que se encarga de mostrar las letras 1 a 1 hasta que se completen todos los array de caracteres
+        for (int i = 0; i < letters.Length; i++)
         {
-            Console.WriteLine("Hello World!");
+            name += letters[i];
+            a[i] = i + 1;
+            SendMessage(name, a[i]);
         }
+        Console.ReadKey();
+    }
+    static void SendMessage(string name, int msg)
+    {
+        //muestra por pantalla el mensaje que se especifica en la siguiente linea y va cambiando los valores name y msg, ya que son varibles
+        Console.WriteLine("Hello, " + name + "! Count to " + msg);
     }
 }
